dfe1b805bba2af7926beb87475bf2395db4bb84f
idobj <idobj@naver.com>
2024-06-22 20:12:58 +0900

    Third code

--- First.cpp
+++ First.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class Time {
-    public:
-            int hour=0;
-            int minute=0;
-    public:
-/*          Time() {
-                 hour=12;
-                    minute=30;
-            } */
-            Time(int h, int m) : hour(h),minute(m){} 
-//             hour = h;
-//             minute = m;
-//            }
-            void print() {
-                    cout << hour << ":" << minute << endl;
-            }
+class Time
+{
+public:
+        int hour = 0;
+        int minute = 0;
+
+public:
+        /*          Time() {
+                         hour=12;
+                            minute=30;
+                    } */
+        Time(int h, int m) : hour(h), minute(m) {}
+        //             hour = h;
+        //             minute = m;
+        //            }
+        void print()
+        {
+                cout << hour << ":" << minute << endl;
+        }
 };
 
-            int main(){
-            Time* time = new Time(12,30);
-            time->print();
-            delete time;
-            }
+int main()
+{
+        Time *time = new Time(12, 30);
+        time->print();
+        delete time;
+} 
